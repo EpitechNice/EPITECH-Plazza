ee9306e7d5e063c31fecf29aeed0cad1b1a972a4
dam <damien.defer@epitech.eu>
2024-05-21 09:12:55 +0200

    [~] Update main to get all numbs from start command

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
-// main.cpp
-#include "includes.hpp"
-#include "user.hpp"
-#include "manager.hpp"
-#include "kitchen.hpp"
-#include "chef.hpp"
-#include "serveur.hpp"
+#include "User.hpp"
+#include "Manager.hpp"
+#include <iostream>
+#include <cstdlib>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <multiplier> <numChefs> <restockTime>" << std::endl;
+        return 1;
+    }
+
+    int numChefs = std::atoi(argv[2]);
+    int restockTime = std::atoi(argv[3]);
+
+    Manager::getInstance().setNumChefs(numChefs);
+    Manager::getInstance().setRestockTime(restockTime);
+
     User user;
-    user.takeOrder("regina XXL x2");
-    
+    user.readCommand();
+
     return 0;
 }
